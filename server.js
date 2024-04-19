17ef5e8fe2ea25000dd7e1e51c87126280f5a3da
阿杰 <53169820+laihaojie@users.noreply.github.com>
2024-04-19 18:14:09 +0800

    feat: 改为本地ip

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 const exec = require('child_process').exec;
+const os = require('os');
 
 const server = http.createServer((req, res) => {
   // 解决跨域问题
@@ -10,7 +11,7 @@ const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   // 获取当前目录路径
   const directoryPath = __dirname;
-  const whiteFileList = ['index.html', 'server.js', 'start.bat','.gitignore', '.gitattributes']
+  const whiteFileList = ['index.html', 'server.js', 'start.bat', '.gitignore', '.gitattributes', 'template.html']
   const whiteFolderList = ['.git']
   const list = [];
 
@@ -43,5 +44,38 @@ const server = http.createServer((req, res) => {
 const PORT = 665;
 server.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
+
+  let ip = '';
+  // 获取本地ip地址
+  // 获取本地网络接口列表
+  const networkInterfaces = os.networkInterfaces();
+
+  // 遍历网络接口列表，找到IPv4地址
+  Object.keys(networkInterfaces).forEach(interfaceName => {
+    const interfaces = networkInterfaces[interfaceName];
+    interfaces.forEach(interfaceInfo => {
+      // 找到IPv4地址
+      if (interfaceInfo.family === 'IPv4' && !interfaceInfo.internal) {
+        console.log(`本地 IP 地址为: ${interfaceInfo.address}`);
+        if (interfaceInfo.address.startsWith('192.168.')) {
+          ip = interfaceInfo.address;
+          return
+        }
+        if (!ip)
+          ip = interfaceInfo.address;
+      }
+    });
+  });
+
+  // 读取template.html文件内容
+  const templatePath = path.join(__dirname, 'template.html');
+  const templateContent = fs.readFileSync(templatePath, 'utf-8');
+
+  // 写入index.html文件
+  const indexPath = path.join(__dirname, 'index.html');
+  const indexContent = templateContent.replaceAll('localhost', ip)
+  fs.writeFileSync(indexPath, indexContent);
+
+
   exec('serve -p 666');
 });
