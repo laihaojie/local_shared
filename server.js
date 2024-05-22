be5962282476cd39fcc6be08bae92932e06d87ad
阿杰 <53169820+laihaojie@users.noreply.github.com>
2024-05-22 20:39:00 +0800

    chore: update

--- server.js
+++ server.js
@@ -9,35 +9,17 @@ const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  // 获取当前目录路径
-  const directoryPath = __dirname;
-  const whiteFileList = ['index.html', 'server.js', 'start.bat', '.gitignore', '.gitattributes', 'template.html', 'README.md']
-  const whiteFolderList = ['.git']
-  const list = [];
 
-  // 递归读取文件夹下的所有文件
-  function readDirSync(dir, list, parentPath = '') {
-    const files = fs.readdirSync(dir);
-    files.forEach((file) => {
-      const filePath = path.join(dir, file);
-      const stat = fs.statSync(filePath);
-      if (stat && stat.isDirectory()) {
-        if (whiteFolderList.includes(file)) return;
-        readDirSync(filePath, list, path.join(parentPath, file));
-      } else {
-        if (whiteFileList.includes(file)) return;
-        list.push(path.join(parentPath, file));
-      }
-    });
+  // 获取请求方法和请求路径
+  const { method, url } = req;
+  if (method === "GET" && url === "/") {
+    handleGet(req, res)
   }
 
-  readDirSync(directoryPath, list);
+  if(method === "POST" && url === "/") {
+    handlePost(req, res)
+  }
 
-  // 设置响应头并发送文件列表给客户端
-  res.writeHead(200, { 'Content-Type': 'application/json' });
-
-
-  res.end(JSON.stringify(list));
 
 });
 
@@ -83,3 +65,39 @@ server.listen(PORT, () => {
 
   exec('serve -p 666');
 });
+
+
+function handleGet(req, res) {
+  // 获取当前目录路径
+  const directoryPath = __dirname;
+  const whiteFileList = ['index.html', 'server.js', 'start.bat', '.gitignore', '.gitattributes', 'template.html', 'README.md']
+  const whiteFolderList = ['.git']
+  const list = [];
+
+  // 递归读取文件夹下的所有文件
+  function readDirSync(dir, list, parentPath = '') {
+    const files = fs.readdirSync(dir);
+    files.forEach((file) => {
+      const filePath = path.join(dir, file);
+      const stat = fs.statSync(filePath);
+      if (stat && stat.isDirectory()) {
+        if (whiteFolderList.includes(file)) return;
+        readDirSync(filePath, list, path.join(parentPath, file));
+      } else {
+        if (whiteFileList.includes(file)) return;
+        list.push(path.join(parentPath, file));
+      }
+    });
+  }
+
+  readDirSync(directoryPath, list);
+
+  // 设置响应头并发送文件列表给客户端
+  res.writeHead(200, { 'Content-Type': 'application/json' });
+
+
+  res.end(JSON.stringify(list));
+
+}
+
+function handlePost(req, res) {}
